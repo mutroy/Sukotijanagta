feb2aeb97ecb3e434d54103f9f94d932d08cb30e
Mohan Uppaluri <101350126+mutroy@users.noreply.github.com>
2023-09-16 16:08:05 -0400

    DAD CH02 Program 1 Added Second Statement

--- DAD_CHAP2.java
+++ DAD_CHAP2.java
@@ -1,5 +1,9 @@
 public class DAD_CHAP2 {
     public static void main(String[] args) {
         System.out.println("NARPAVI AMMA");
+
+        //Modifying the program by adding one more statement
+
+        System.out.println("VARAHI AMMA");
     }
 }
