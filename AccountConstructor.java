81870b9dda52caa5fc5284c746867a9a2ba9fbfc
Mohan Uppaluri <101350126+mutroy@users.noreply.github.com>
2023-09-23 17:24:08 -0400

    DAD CH03 Constructor Practice

---
+++ AccountConstructor.java
@@ -0,0 +1,17 @@
+public class AccountConstructor {
+
+    private String name;
+
+    public AccountConstructor (String name){
+        this.name = name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+}
---
+++ AccountConstructorTest.java
@@ -0,0 +1,10 @@
+public class AccountConstructorTest {
+
+    public static void main(String[] args) {
+
+        AccountConstructor account1 = new AccountConstructor("NARPAVI");
+        AccountConstructor account2 = new AccountConstructor("NIRVITA");
+
+        System.out.println(" The names are "+ account1.getName() + " & " + account2.getName());
+    }
+}
