a5d1c225483b5aede2abc7a5618696bb239046ee
Mohan Uppaluri <101350126+mutroy@users.noreply.github.com>
2023-09-23 22:13:13 -0400

    DAD CH03 Added Withdraw amount exercise question 8 solution

--- Account.java
+++ Account.java
@@ -6,25 +6,35 @@ public class Account {
     public Account(String name,Double balance){
         this.name = name;
 
-        if(balance > 0.0) {
+        if (balance > 0.0) {
             this.balance = balance;
         }
     }
 
-    public void deposit(Double depositAmount){
-        if(depositAmount > 0.0){
+    public void deposit(Double depositAmount) {
+        if (depositAmount > 0.0) {
             balance = balance + depositAmount;
         }
     }
 
-    public double getBalance(){
-        return balance;
-    }
-    public void setName(String name){
-        this.name = name;
+    public void withDraw(Double withdrawAmount) {
+        if (withdrawAmount > balance) {
+            System.out.println("Insufficient Balance");
+        } else {
+            balance = balance - withdrawAmount;
+            System.out.printf(" Current Balance after withdrawal is %.2f ", this.balance);
+        }
     }
 
-    public String getName(){
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
--- AccountTest.java
+++ AccountTest.java
@@ -13,13 +13,18 @@ public class AccountTest {
         System.out.print("Enter deposit amount for " + account1.getName() + " account : ");
         double depositAmount = input.nextDouble();
 
-        System.out.printf("%n Adding %.2f to account1 balance%n%n",depositAmount);
+        System.out.printf("%n Adding %.2f to account1 balance%n%n", depositAmount);
         account1.deposit(depositAmount);
 
         System.out.printf("%s balance: $%.2f%n",
                 account1.getName(), account1.getBalance());
         System.out.printf("%s balance: $%.2f%n%n",
                 account2.getName(), account2.getBalance());
+
+        System.out.print(" Enter the amount for withdrawal: ");
+        Double withdrawalAmount = input.nextDouble();
+
+        account1.withDraw(withdrawalAmount);
 
 
     }
